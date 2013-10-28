10f108836ad0ac777bf6783c24cf048d0170dcf8
Qi Yang <i@yangqi.me>
2013-10-27 22:02:33 -0400

    init commit

---
+++ src/Yangqi/Htmldom/HtmldomServiceProvider.php
@@ -0,0 +1,47 @@
+<?php namespace Yangqi\Htmldom;
+
+use Illuminate\Support\ServiceProvider;
+
+class HtmldomServiceProvider extends ServiceProvider {
+
+	/**
+	 * Indicates if loading of the provider is deferred.
+	 *
+	 * @var bool
+	 */
+	protected $defer = false;
+
+	/**
+     * Bootstrap the application events.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        $this->package('yangqi/htmldom');
+    }
+
+	/**
+	 * Register the service provider.
+	 *
+	 * @return void
+	 */
+	public function register()
+	{
+		$this->app['htmldom'] = $this->app->share(function($app)
+        {
+            return new Htmldom;
+        });
+	}
+
+	/**
+	 * Get the services provided by the provider.
+	 *
+	 * @return array
+	 */
+	public function provides()
+	{
+		return array();
+	}
+
+}
